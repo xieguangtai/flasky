a85d80fc6fefed908f76e00d7bf3e5e3a1d5f553
xieguangtai <xieguangtai@outlook.com>
2017-06-14 21:13:58 +0800

    add 2c

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 #coding: utf8
 from flask import Flask
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
