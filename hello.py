75c2126c1f06bbc5f7b8d7a5c845a553524b4fbe
xieguangtai <xieguangtai@outlook.com>
2017-06-25 17:00:35 +0800

    5c

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_script import Manager
+from flask import Flask, render_template, session, redirect, url_for
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -48,6 +48,11 @@ class NameForm(FlaskForm):
     submit = SubmitField('Sumbit')
 
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -62,12 +67,17 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                            known=session.get('known', False))
 
 
 if __name__ == '__main__':
